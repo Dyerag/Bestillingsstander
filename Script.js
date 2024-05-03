833ac6a7642ac844fc580884b0d79a47e6b30a12
dyerag <dylaneric2003@gmail.com>
2024-05-03 13:06:17 +0200

    Fixed a small mistake in the script

--- Script.js
+++ Script.js
@@ -2,7 +2,7 @@ let customerNumber = 1;
 
 function updateCustomerNumber() {
     document.getElementById('customerNumber').innerHTML = '';
-    let customerElement = document.createElement('h3    ');
+    let customerElement = document.createElement('h3');
     customerElement.innerText = customerNumber;
     let customerLocation = document.getElementById('customerNumber');
     customerLocation.appendChild(customerElement);
