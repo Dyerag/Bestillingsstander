877d89bd58b9b8b441522e561edd33c423c2e30d
dyerag <dylaneric2003@gmail.com>
2024-05-03 13:00:59 +0200

    Finished most of the basic layout. I've also started writing the javascript. So far, i've made a variable to keep track of customers number, and update it, as well as tested that it indeed works. Just need to have it make a space inbetween, and it's of to writing the menu option.

---
+++ Script.js
@@ -0,0 +1,14 @@
+let customerNumber = 1;
+
+function updateCustomerNumber() {
+    document.getElementById('customerNumber').innerHTML = '';
+    let customerElement = document.createElement('h3    ');
+    customerElement.innerText = customerNumber;
+    let customerLocation = document.getElementById('customerNumber');
+    customerLocation.appendChild(customerElement);
+}
+
+updateCustomerNumber();
+
+customerNumber = 7;
+updateCustomerNumber();
