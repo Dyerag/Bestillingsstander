7d7b241a6f7a3e842ed0676960e5af97aa5b0c78
dyerag <dylaneric2003@gmail.com>
2024-05-07 14:10:54 +0200

    Currently trying to add functionality to page.

--- Script.js
+++ Script.js
@@ -8,89 +8,107 @@ const mainCourses = [
     {
         name: "HOMESTYLE PEBERSAUCE",
         price: 77,
-        section: "maincourse"
+        section: "maincourse",
+        id: 1
     },
     {
         name: "DOUBLE CHEESEBURGER",
         price: 30,
-        section: "maincourse"
+        section: "maincourse",
+        id: 2
     },
     {
         name: "BIG MAC MENU",
         price: 103,
-        section: "maincourse"
+        section: "maincourse",
+        id: 3
     },
     {
         name: "2 x DIJON CHICKEN CHEESE MENU",
         price: 96,
-        section: "maincourse"
+        section: "maincourse",
+        id: 4
     },
     {
         name: "MIXED TEAMBOX MENU",
         price: 124,
-        section: "maincourse"
+        section: "maincourse",
+        id: 5
     }
 ];
 const drinks = [{
     name: "COCA-COLA",
     price: 32,
-    section: "drink"
+    section: "drink",
+    id: 6
 }, {
     name: "FUZE TEA FERSKEN",
     price: 32,
-    section: "drink"
+    section: "drink",
+    id: 7
 }, {
     name: "MILKSHAKE M. JORDBÆRSMAG",
     price: 35,
-    section: "drink"
+    section: "drink",
+    id: 8
 }, {
     name: "MILKSHAKE M. CHOKOLADESMAG",
     price: 35,
-    section: "drink"
+    section: "drink",
+    id: 9
 }, {
     name: "SHAKESPRESSO",
     price: 42,
-    section: "drink"
+    section: "drink",
+    id: 10
 }
 ];
 const desserts = [{
     name: "SUNDAE MED KARAMEL",
     price: 26,
-    section: "desert"
+    section: "desert",
+    id: 11
 }, {
     name: "MCFLURRY KARAMEL & POPCORNSMAG",
     price: 35,
-    section: "desert"
+    section: "desert",
+    id: 12
 }, {
     name: "OREO MUFFIN",
     price: 20,
-    section: "desert"
+    section: "desert",
+    id: 13
 }, {
     name: "CHOKOLADE DONUT",
     price: 13,
-    section: "desert"
+    section: "desert",
+    id: 14
 },
 ];
 const accessories = [{
     name: "POMMES FRITES",
     price: 31,
-    section: "addition"
+    section: "addition",
+    id: 15
 }, {
     name: "BARBEQUE",
     price: 8,
-    section: "addition"
+    section: "addition",
+    id: 16
 }, {
     name: "SPICY CHICKEN MCNUGGETS 6 stk.",
     price: 47,
-    section: "addition"
+    section: "addition",
+    id: 17
 }, {
     name: "CHILI CHEESE TOPS TEAM BOX",
     price: 69,
-    section: "addition"
+    section: "addition",
+    id: 18
 },
 ];
 /*List of items picke by the user*/
-let orderList=[];
+let orderList = [];
 
 /*Writes the customer number at the top of the page after incremanting it by 1*/
 function updateCustomerNumber() {
@@ -104,41 +122,73 @@ function updateCustomerNumber() {
 }
 
 function renderMenu(menuList) {
-    menuList.forEach(function (Item) {
+    menuList.forEach(function (item) {
         /*The items are made a button element, because button already has all the needed funtions*/
         let option = document.createElement('button');
-        let elementLocation = document.getElementById(Item.section);
-        /*Gives the menu item the item class */
+        let elementLocation = document.getElementById(item.section);
+        /*Gives the menu item the class called item*/
         option.setAttribute('class', 'item');
 
         let optionName = document.createElement('h5');
-        optionName.innerText = Item.name;
+        optionName.innerText = item.name;
         optionName.setAttribute('class', 'itemName');
 
         let optionPrice = document.createElement('div');
-        optionPrice.innerText = "Pris: " + Item.price;
+        optionPrice.innerText = "Pris: " + item.price;
         /*The Price also gets a class, just in case i need to change the styling of it later */
         optionPrice.setAttribute('class', 'itemPrice');
 
         option.append(optionName, optionPrice);
+        option.onclick = addToOrderList;
+        option.id = item.id
 
         elementLocation.appendChild(option);
-    })
+    });
 };
 
+function addToOrderList(event) {
+    console.log(event);
+
+    renderOrderlist();
+}
+
+function renderOrderlist() {
+    // document.getElementById('cartItems').innerText = "";
+    let cart = document.getElementById('cartItems');
+    cart.innerHTML = '';
+
+    let content = document.createElement('div');
+
+    if (orderList.length == 0) {
+        /*Make it write that there is nothing in the cart when the cart is empty*/
+        content.innerText = 'Der er ikke lagt noget i kurven';
+        content.setAttribute('id', 'empty');
+
+        cart.appendChild(content);
+    }
+    else {
+        /* Render the contents of the cart on the cart section of the page*/
+        orderList.forEach(function (item) {
+            const remove = document.createElement('button');
+
+            remove.setAttribute('class', "remove");
+            remove.innerText = '-';
+            remove.onclick = removeFromCart;
+
+            item.appendChild(remove);
+        });
+    };
+}
+
+function removeFromCart() {
+
+    renderOrderlist();
+}
+
+renderOrderlist();
 updateCustomerNumber();
 
 renderMenu(mainCourses);
 renderMenu(drinks);
 renderMenu(desserts);
-renderMenu(accessories)
-
-function RenderOrderlist() {
-    if (orderList.length =0) {
-        /* Render the contents of the cart on the cart section of the page*/
-    }
-    else {
-        /*Make it write that there is nothing in the cart when the cart is empty*/
-
-    }
-}
+renderMenu(accessories);
