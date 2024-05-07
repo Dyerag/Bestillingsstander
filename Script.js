ebbf67ee472e3f2cf49c461a24b69cbc0b46808f
dyerag <dylaneric2003@gmail.com>
2024-05-07 10:35:01 +0200

    Made the whitespace between custemernumber, and the actual value work. The layout for the menu section is just about done aswell. Might add images of the menu items later, but only if theres time. Speaking of menu items. The Function that wrote them has been change to use a parameter, instead of having a loop for each array in it, cus' that was stupid. Finally, the menu items are being made as butten elements instead of div.

--- Script.js
+++ Script.js
@@ -1,74 +1,101 @@
-let customerNumber = 1;
+let customerNumber = 0; /*starts at zero, because the function that writes it on the page, begins by increasing it by 1.*/
+
+/*The menu items has been hardcoded into the script, because i couldn't get the script to read a json file.*/
+/*All of the object variables have the same name, so that they can all be run through the rendermMenu function*/
+/*All the objects have a section variable, so that the renderMenu function knows
+where to put them on the page once they're made into elements */
 const mainCourses = [
     {
-        Meal: "HOMESTYLE PEBERSAUCE",
-        price: 77
+        name: "HOMESTYLE PEBERSAUCE",
+        price: 77,
+        section: "maincourse"
     },
     {
-        Meal: "DOUBLE CHEESEBURGER",
-        price: 30
+        name: "DOUBLE CHEESEBURGER",
+        price: 30,
+        section: "maincourse"
     },
     {
-        Meal: "BIG MAC MENU",
-        price: 103
+        name: "BIG MAC MENU",
+        price: 103,
+        section: "maincourse"
     },
     {
-        Meal: "2 x DIJON CHICKEN CHEESE MENU",
-        price: 96
+        name: "2 x DIJON CHICKEN CHEESE MENU",
+        price: 96,
+        section: "maincourse"
     },
     {
-        Meal: "MIXED TEAMBOX MENU",
-        price: 124
+        name: "MIXED TEAMBOX MENU",
+        price: 124,
+        section: "maincourse"
     }
 ];
 const drinks = [{
-    drink: "COCA-COLA",
-    price: 32
+    name: "COCA-COLA",
+    price: 32,
+    section: "drink"
 }, {
-    drink: "FUZE TEA FERSKEN",
-    price: 32
+    name: "FUZE TEA FERSKEN",
+    price: 32,
+    section: "drink"
 }, {
-    drink: "MILKSHAKE M. JORDBÆRSMAG",
-    price: 35
+    name: "MILKSHAKE M. JORDBÆRSMAG",
+    price: 35,
+    section: "drink"
 }, {
-    drink: "MILKSHAKE M. CHOKOLADESMAG",
-    price: 35
+    name: "MILKSHAKE M. CHOKOLADESMAG",
+    price: 35,
+    section: "drink"
 }, {
-    drink: "SHAKESPRESSO",
-    price: 42
+    name: "SHAKESPRESSO",
+    price: 42,
+    section: "drink"
 }
-]
+];
 const desserts = [{
-    desert: "SUNDAE MED KARAMEL",
-    price: 26
+    name: "SUNDAE MED KARAMEL",
+    price: 26,
+    section: "desert"
 }, {
-    desert: "MCFLURRY KARAMEL & POPCORNSMAG",
-    price: 35
+    name: "MCFLURRY KARAMEL & POPCORNSMAG",
+    price: 35,
+    section: "desert"
 }, {
-    desert: "OREO MUFFIN",
-    price: 20
+    name: "OREO MUFFIN",
+    price: 20,
+    section: "desert"
 }, {
-    desert: "CHOKOLADE DONUT",
-    price: 13
+    name: "CHOKOLADE DONUT",
+    price: 13,
+    section: "desert"
 },
-]
+];
 const accessories = [{
-    accessory: "POMMES FRITES",
-    price: 31
+    name: "POMMES FRITES",
+    price: 31,
+    section: "addition"
 }, {
-    accessory: "BARBEQUE",
-    price: 8
+    name: "BARBEQUE",
+    price: 8,
+    section: "addition"
 }, {
-    accessory: "SPICY CHICKEN MCNUGGETS 6 stk.",
-    price: 47
+    name: "SPICY CHICKEN MCNUGGETS 6 stk.",
+    price: 47,
+    section: "addition"
 }, {
-    accessory: "CHILI CHEESE TOPS TEAM BOX",
-    price: 69
+    name: "CHILI CHEESE TOPS TEAM BOX",
+    price: 69,
+    section: "addition"
 },
-]
-let orderList;
+];
+/*List of items picke by the user*/
+let orderList=[];
 
+/*Writes the customer number at the top of the page after incremanting it by 1*/
 function updateCustomerNumber() {
+    customerNumber++;
+    /*This code Clears the customernumber, so that the new one can be written*/
     document.getElementById('customerNumber').innerHTML = '';
     let customerElement = document.createElement('h3');
     customerElement.innerText = customerNumber;
@@ -76,32 +103,22 @@ function updateCustomerNumber() {
     customerLocation.appendChild(customerElement);
 }
 
-function renderMenu() {
-    mainCourses.forEach(function (course) {
-        let option = document.createElement('div');
-        let elementLocation = document.getElementById('maincourse');
+function renderMenu(menuList) {
+    menuList.forEach(function (Item) {
+        /*The items are made a button element, because button already has all the needed funtions*/
+        let option = document.createElement('button');
+        let elementLocation = document.getElementById(Item.section);
+        /*Gives the menu item the item class */
+        option.setAttribute('class', 'item');
 
-        let optionName = document.createElement('h4');
-        optionName.innerText = course.Meal;
-        option.setAttribute('class','itemName')
-
-        let optionPrice = document.createElement('div');
-        optionPrice.innerText = "Pris: " + course.price;
-
-        option.append(optionName, optionPrice);
-
-        elementLocation.appendChild(option);
-    })
-
-    drinks.forEach(function (drink) {
-        let option = document.createElement('div');
-        let elementLocation = document.getElementById('drink');
-
-        let optionName = document.createElement('h4');
-        optionName.innerText = drink.drink;
+        let optionName = document.createElement('h5');
+        optionName.innerText = Item.name;
+        optionName.setAttribute('class', 'itemName');
 
         let optionPrice = document.createElement('div');
-        optionPrice.innerText = "Pris: " + drink.price;
+        optionPrice.innerText = "Pris: " + Item.price;
+        /*The Price also gets a class, just in case i need to change the styling of it later */
+        optionPrice.setAttribute('class', 'itemPrice');
 
         option.append(optionName, optionPrice);
 
@@ -109,11 +126,19 @@ function renderMenu() {
     })
 };
 
-renderMenu();
+updateCustomerNumber();
+
+renderMenu(mainCourses);
+renderMenu(drinks);
+renderMenu(desserts);
+renderMenu(accessories)
 
 function RenderOrderlist() {
-    if (true) {
-
+    if (orderList.length =0) {
+        /* Render the contents of the cart on the cart section of the page*/
+    }
+    else {
+        /*Make it write that there is nothing in the cart when the cart is empty*/
 
     }
 }
